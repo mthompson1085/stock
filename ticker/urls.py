7c68e347b1a4b317cec4870a5bbb83b099ae2cb0
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-10-08 20:41:52 -0400

    added about page to app

--- ticker/urls.py
+++ ticker/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
 
 ]
 
