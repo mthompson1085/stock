a8869e725627417702a7bcfcc69011cb75a18d37
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-10-09 19:41:47 -0400

    added api json call

--- ticker/views.py
+++ ticker/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json 
+
+    #pk_61ca93bb27b54f39915de255562e1cdd
+    api_request = requests.get ("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_61ca93bb27b54f39915de255562e1cdd")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api= "Error..."
+
+
+    return render(request, 'home.html', {'api': api})
 
 def about (request):
     return  render(request, 'about.html', {})
