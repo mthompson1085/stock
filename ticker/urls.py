de739d5730e81b1dd267a682a71345441485f9e2
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-10-09 20:33:07 -0400

    added in the stock serch function

--- ticker/urls.py
+++ ticker/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
+    path('add_stock.html', views.add_stock, name="add_stock")
 
 ]
 
--- ticker/views.py
+++ ticker/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 
+
 def home(request):
     import requests
     import json 
 
-    #pk_61ca93bb27b54f39915de255562e1cdd
-    api_request = requests.get ("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_61ca93bb27b54f39915de255562e1cdd")
+    if request.method == 'POST':
+        ticker = request.POST ['ticker']
+        api_request = requests.get ("https://cloud.iexapis.com/stable/stock/"  + ticker + "/quote?token=pk_61ca93bb27b54f39915de255562e1cdd")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api= "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api= "Error..."
+        return render(request, 'home.html', {'api': api})
+            
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Stock Symbol Above .."})
+        
 
-
-    return render(request, 'home.html', {'api': api})
+    
 
 def about (request):
     return  render(request, 'about.html', {})
