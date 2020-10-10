80f3c8d6a6d4137e8e0195a8f4eb869ecdeaf805
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-10-10 15:05:23 -0400

    updated views to add delete stock view

--- ticker/urls.py
+++ ticker/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
     path('add_stock.html', views.add_stock, name="add_stock"),
-    path('delete/<stock_id>', views.delete, name="delete")
+    path('delete/<stock_id>', views.delete, name="delete"),
+    path('delete_stock.html', views.delete_stock, name="delete_stock"),
+
 ]
 
 
