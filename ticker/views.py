5111d9da9520b4eb6bdff19d66706eaebea1ad3f
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-10-10 11:08:29 -0400

    updated for loop

--- ticker/views.py
+++ ticker/views.py
@@ -27,6 +27,9 @@ def about (request):
     return  render(request, 'about.html', {})
 
 def add_stock(request):
+    import requests
+    import json 
+
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -36,7 +39,15 @@ def add_stock(request):
             return redirect ('add_stock')
     else:
         ticker = Stock.objects.all()
-        return  render(request, 'add_stock.html', {'ticker': ticker})
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/"  + str(ticker_item) + "/quote?token=pk_61ca93bb27b54f39915de255562e1cdd")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api= "Error..."
+        return  render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
