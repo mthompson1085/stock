562cfd0c9fb1d3eb4c81db7586eae94b4b441664
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-10-10 15:05:55 -0400

    changed delete redirect

--- ticker/views.py
+++ ticker/views.py
@@ -49,8 +49,12 @@ def add_stock(request):
                 api= "Error..."
         return  render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
 
-def delete(request, stock_id):
+def delete (request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ("Stock has been deleted"))
-    return redirect (add_stock)
+    return redirect (delete_stock)
+
+def delete_stock (request):
+    ticker = Stock.objects.all()
+    return  render(request, 'delete_stock.html', {'ticker':ticker})
